306f95faf92a10f24d0bae091eb5e13a768f8a70
Gunaseelanselvam <gunaseelanvec@gmail.com>
2026-08-20 16:06:28 +0530

    Create StudentController.java

---
+++ src/main/java/com/college_management/college/controller/StudentController.java
@@ -0,0 +1,35 @@
+package com.college_management.college.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.college_management.college.dto.StudentRequest;
+import com.college_management.college.entity.Student;
+import com.college_management.college.service.StudentService;
+
+import io.micrometer.core.ipc.http.HttpSender.Response;
+
+
+@RestController
+@RequestMapping("/api/students")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService){
+        this.studentService = studentService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Student> createStudent(@RequestBody StudentRequest request){
+
+        Student savedStudent = studentService.createStudent(request);
+        return ResponseEntity.ok(savedStudent);
+
+    }
+    
+}
